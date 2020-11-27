421259e284d4238a8ca864f61cf525a0747c09fc
Kevin Carter <kbc273@nyu.edu>
2020-11-27 13:26:29 -0500

    Added app clip

---
+++ MiauMaruAppClip/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MiauMaruAppClip
+//
+//  Created by Kevin Carter on 11/23/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
